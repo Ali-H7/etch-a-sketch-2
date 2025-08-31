a2e5ff1727b81be485225455da5a4474cec9d237
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-08-31 07:55:37 +0300

    Create function to set the opacity of colors

--- script.js
+++ script.js
@@ -5,27 +5,41 @@ function createSquares(squareCount = 16) {
     for (let i = 0; i < squareCount; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.dataset.opacity = '0'
         square.style.width = `${squareSize}` + "px";
         square.style.height = `${squareSize}` + "px";
-        // addColorOnHover(square);
+        addColorOnHover(square);
         container.appendChild(square);
     }
 }
 
-// function addColorOnHover(squareElement) {
-//     squareElement.addEventListener('mouseenter', () => setColors(squareElement));
-// };
+function addColorOnHover(squareElement) {
+    squareElement.addEventListener('mouseenter', () => setColors(squareElement));
+};
 
-// function getColors() {
-//     const r = Math.floor(Math.random() * 255) + 1;
-//     const g = Math.floor(Math.random() * 255) + 1;
-//     const b = Math.floor(Math.random() * 255) + 1;
-//     return [r, g, b];
-// }
+function getColors() {
+    const r = Math.floor(Math.random() * 255) + 1;
+    const g = Math.floor(Math.random() * 255) + 1;
+    const b = Math.floor(Math.random() * 255) + 1;
+    return [r, g, b];
+}
 
-// function setColors(squareElement) {
-//     const colors = getColors();
-//     squareElement.style.backgroundColor = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
-// }
+function setColors(squareElement) {
+    const colors = getColors();
+    const opacity = setOpacity(squareElement);
+    squareElement.style.backgroundColor = `rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, ${opacity} )`;
+}
+
+function setOpacity(squareElement) {
+    const currentOpacity = squareElement.dataset.opacity;
+    let newOpacity = null;
+    if (currentOpacity > 1) {
+        newOpacity = 1;
+    } else {
+        newOpacity = Number(currentOpacity) + 0.1;
+        squareElement.dataset.opacity = newOpacity;
+    }
+    return newOpacity;
+};
 
 createSquares();
